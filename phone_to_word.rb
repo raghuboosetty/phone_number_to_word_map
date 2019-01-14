dd074ece0c6718f3b89c873e77a2b75338d8f5e2
Raghu Boosetty <raghuboosetty@gmail.com>
2019-01-14 19:54:03 +0530

    initial commit with basic conversion of words to numbers of given dictornary and phone number

---
+++ phone_to_word.rb
@@ -0,0 +1,62 @@
+class PhoneToWord
+  def initialize(options={})
+    @min_word_length = options[:min_word_length] || 3
+    @max_phone_length = options[:max_phone_length] || 10
+
+    @word_hash = {}
+    puts 'Initializing...'
+    File.read("dictionary.txt").split("\n").each do |word|
+      @word_hash[word] = ''
+      word.split('').each do |letter|
+        case letter.downcase
+          when 'a', 'b', 'c'
+            @word_hash[word] += '2'
+          when 'd', 'e', 'f'
+            @word_hash[word] += '3'
+          when 'g', 'h', 'i'
+            @word_hash[word] += '4'
+          when 'j', 'k', 'l'
+            @word_hash[word] += '5'
+          when 'm', 'n', 'o'
+            @word_hash[word] += '6'
+          when 'p', 'q', 'r', 's'
+            @word_hash[word] += '7'
+          when 't', 'u', 'v'
+            @word_hash[word] += '8'
+          when 'w', 'x', 'y', 'z'
+            @word_hash[word] += '9'
+        end
+      end
+      # word_hash[word] = word_hash[word].to_i
+    end
+    @word_hash
+
+    @phone_number = nil
+    while !valid_phone?(@phone_number)
+      puts "Please enter a 10 digit phone number without 0's and 1's:"
+      @phone_number = gets.chomp
+    end
+  end
+
+  def valid_phone?(phone_number)
+    phone_number && phone_number.length == 10 && phone_number.match(/^[2-9]*$/)
+  end
+
+  def phone_number_combinations(phone_number = @phone_number)
+    combination = (phone_number.length < @min_word_length) ? [] : [[phone_number]]
+    max = [@max_phone_length, phone_number.length].min
+    (@min_word_length...max).inject(combination) do |combination, i|
+      phone_number_combinations(phone_number[i..-1]).inject(combination) do |combination, tail|
+        combination.push([phone_number[0...i]] + tail)
+      end
+    end
+  end
+
+  def to_words
+    puts phone_number_combinations.inspect
+  end
+end
+
+phone_to_word = PhoneToWord.new
+
+phone_to_word.to_words
